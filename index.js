0c8f8f0db753bc64b1f93635a028b2b1ba7c7a26
shjroemon <zzdaotuanhuyzz@gmail.com>
2023-09-29 16:36:14 +0700

    fix

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
-const mongoose = require("mongoose");
-const dbConfig = require("./config/dbConfig");
+const cors = require('cors'); // Import the cors package
+require('dotenv').config();
+const mongoose = require('mongoose');
+const dbConfig = require('./config/dbConfig');
 const port = process.env.PORT || 8000;
 
-const usersRoute = require("./routes/usersRoute");
-const projectsRoute = require("./routes/projectsRoute");
-const tasksRoute = require("./routes/tasksRoute");
-const notificationsRoute = require("./routes/notificationsRoute");
+const usersRoute = require('./routes/usersRoute');
+const projectsRoute = require('./routes/projectsRoute');
+const tasksRoute = require('./routes/tasksRoute');
+const notificationsRoute = require('./routes/notificationsRoute');
 
+app.use(cors()); // Use the cors middleware
 app.use(express.json());
 
 // Connect to MongoDB
@@ -18,14 +20,14 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
-app.use("/api/users", usersRoute);
-app.use("/api/projects", projectsRoute);
-app.use("/api/tasks", tasksRoute);
-app.use("/api/notifications", notificationsRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/projects', projectsRoute);
+app.use('/api/tasks', tasksRoute);
+app.use('/api/notifications', notificationsRoute);
 
-// deployment config
+// Deployment config
 
 app.listen(port, () => console.log(`Node JS server listening on port ${port}`));
