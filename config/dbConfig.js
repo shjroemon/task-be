88a4a621a0656da98b381434e755d744b74fcaba
shjroemon <zzdaotuanhuyzz@gmail.com>
2023-09-29 16:08:07 +0700

    fix code

---
+++ config/dbConfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://shjroemon:daotuanhuy1@cluster0.g48th9i.mongodb.net/?retryWrites=true&w=majority",
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const dbConfig = require("./config/dbConfig");
+const port = process.env.PORT || 8000;
+
+const usersRoute = require("./routes/usersRoute");
+const projectsRoute = require("./routes/projectsRoute");
+const tasksRoute = require("./routes/tasksRoute");
+const notificationsRoute = require("./routes/notificationsRoute");
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(dbConfig.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use("/api/users", usersRoute);
+app.use("/api/projects", projectsRoute);
+app.use("/api/tasks", tasksRoute);
+app.use("/api/notifications", notificationsRoute);
+
+// deployment config
+
+app.listen(port, () => console.log(`Node JS server listening on port ${port}`));
